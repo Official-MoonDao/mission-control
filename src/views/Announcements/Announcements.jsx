137ffb2ca816d9675ea9930d34ec45d72af2015a
Lucas Peñalva <lucasarielpenalva@gmail.com>
2022-10-21 19:06:32 -0300

    Changed Announcement State so changing tabs won't make another request/reload

--- src/views/Announcements/Announcements.jsx
+++ src/views/Announcements/Announcements.jsx
@@ -1,10 +1,27 @@
-import AnnoucementBoard from "./AnnoucementBoard";
-import PageLayout from "../../components/Layout/PageLayout";
+import { PageLayout, SectionLayout } from "../../components/Layout";
+import { errorToast } from "../../hooks&utils/errorToast";
+import { Line } from "../../components/Layout";
+import Announcement from "./Announcement";
+import Header from "../../components/Header";
+import AnnouncementSkeletons from "../../components/Skeletons/AnnouncementSkeletons";
 
-const Announcements = () => {
+const Announcements = ({ announcements, announcementsLoaded, announcementsError }) => {
+  if (announcementsError) errorToast("Connection with Discord failed. Contact MoonDAO if the problem persists 🚀");
+
   return (
     <PageLayout>
-      <AnnoucementBoard />
+      <SectionLayout>
+        <Header text={"Announcements"} />
+        <Line />
+
+        <div className="mt-[34px]">
+          {!announcementsLoaded || announcementsError ? (
+            <AnnouncementSkeletons />
+          ) : (
+            announcements.map((e) => <Announcement key={e.id} content={e.content} mentions={e.mentions} author={e.author} timestamp={e.timestamp} reactions={e.reactions} />)
+          )}
+        </div>
+      </SectionLayout>
     </PageLayout>
   );
 };
