5f5248e0d0745c60de4532e8026ac8b5c9f94ecb
Alejandro Moncada-Larrotiz <amoncada@umich.edu>
2022-10-28 17:47:42 -0700

    removed top pagination bar and added new author mappings for proposals

--- src/api/proposalsGQL.js
+++ src/api/proposalsGQL.js
@@ -23,6 +23,8 @@ query Proposals( $skip:Int) {
 
 export const authorMappings = {
   "0x679d87D8640e66778c3419D164998E720D7495f6": "@pmoncada",
+  "0x5640Ddc028f2436B5C0BA0305D2199556C1b5a95": "@Larrotiz",
+  "0x3c48621a501567463768aE79e6547091418E24Ee": "@kori"
 };
 
 
