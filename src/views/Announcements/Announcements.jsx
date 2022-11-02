27ab1c75e24e727e4f4318240076498207dcd248
Lucas Peñalva <lucasarielpenalva@gmail.com>
2022-11-01 22:08:01 -0300

    Fixed bug when reaching end of announcements infinite scroll

--- src/views/Announcements/Announcements.jsx
+++ src/views/Announcements/Announcements.jsx
@@ -11,7 +11,8 @@ const Announcements = ({ announcements, announcementsLoaded, announcementsError,
   const [updating, setUpdating] = useState(false);
   const [updatingError, setUpdatingError] = useState(false);
   const lastPostId = announcements.length ? announcements[announcements.length - 1].id : "";
-
+  const endingId = "916126920339509268";
+  
   const intObserver = useRef();
   const lastPostRef = useCallback(
     (announcement) => {
@@ -19,7 +20,7 @@ const Announcements = ({ announcements, announcementsLoaded, announcementsError,
       if (intObserver.current) intObserver.current.disconnect();
 
       intObserver.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
+        if (entries[0].isIntersecting && lastPostId !== endingId) {
           updateAnnouncements(lastPostId, announcements, setAnnouncements, setUpdating, setUpdatingError);
         }
       });
@@ -51,7 +52,17 @@ const Announcements = ({ announcements, announcementsLoaded, announcementsError,
                   )
                 )}
 
-              <p className={`${updating ? "block" : "hidden"} text-gradient mt-7 w-full animate-pulse text-center text-xl lg:text-2xl`}>Loading more announcements...</p>
+              {lastPostId !== endingId ? (
+                <p className={`${updating ? "block" : "hidden"} text-gradient mt-7 w-full animate-pulse text-center text-xl lg:text-2xl`}>Loading more announcements...</p>
+              ) : (
+                <button
+                name="Back to the Top"
+                  onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+                  className="bg-gradient-to-r mt-10 from-moon-orange underline decoration-2 hover:scale-105 decoration-rose-400 to-moon-pink bg-clip-text font-semibold text-transparent  dark:from-moon-pink dark:to-moon-gold text-2xl lg:text-3xl"
+                >
+                  Back to the top→
+                </button>
+              )}
             </>
           )}
         </div>
