7e011d365227dedf1af7bd35ec97ab473f68a8af
Lucas Peñalva <lucasarielpenalva@gmail.com>
2022-11-17 17:20:20 -0300

    Handled allowed assets

---
+++ src/utilities/allowedAssets.js
@@ -0,0 +1,9 @@
+export const allowedAssets = {
+  MOONEY: true,
+  DAI: true,
+  ETH: true,
+  JBX: true,
+  USDC: true,
+  USDT: true,
+  SAFE: true,
+};
--- src/views/Treasury/BalanceAndAssets/Assets.jsx
+++ src/views/Treasury/BalanceAndAssets/Assets.jsx
@@ -1,12 +1,12 @@
 import Asset from "./Asset";
 import ArticleTitle from "../../../components/ArticleTitle";
-
+import {allowedAssets} from '../../../utilities/allowedAssets'
 const Assets = ({ tokens }) => {
   return (
     <section className="mt-3 xl:mt-5 flex flex-col">
       <ArticleTitle text={"DAO Assets"} />
 
-      {tokens.map((token, i) => (
+      {tokens.filter(token => allowedAssets[token.symbol]).map((token, i) => (
         <Asset key={i} name={token.symbol} amount={token.balance.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
           usd={token.usd.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} address={token.address}/>
       ))}
